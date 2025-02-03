f7cf6d6809afc98845403ca0b577bc321abc2ac5
ManuelB16 <felipebarrera104@gmail.com>
2025-02-03 16:17:39 -0500

    Inicializacion 2 punto

---
+++ src/main/java/snake/src/main/java/enums/GridSize.java
@@ -0,0 +1,9 @@
+package snake.src.main.java.enums;
+
+public class GridSize {
+
+	public static final int HEIGH_BOX = 20;
+	public static final int WIDTH_BOX = 20;
+	public static final int GRID_WIDTH = 30;
+	public static final int GRID_HEIGHT = 30;
+}
