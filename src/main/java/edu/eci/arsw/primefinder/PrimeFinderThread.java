9a5cc930abfff6600cd7d61efccef41524c03532
ManuelB16 <felipebarrera104@gmail.com>
2025-02-03 14:01:29 -0500

    snake

--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,52 +3,53 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
-
-	int a,b;
-	
-	private List<Integer> primes;
-	private final Object lock;
+public class PrimeFinderThread extends Thread {
+    int a, b;
+    private List<Integer> primes;
+    private final Object lock;
     private boolean paused;
-	public PrimeFinderThread(int a, int b, Object lock) {
-		super();
+
+    public PrimeFinderThread(int a, int b, Object lock) {
+        super();
         this.lock = lock;
         this.primes = new LinkedList<>();
-		this.a = a;
-		this.b = b;
+        this.a = a;
+        this.b = b;
         this.paused = false;
-	}
+    }
 
-        @Override
-	public void run() {
-            for (int i = a; i < b; i++) {
-                synchronized (lock) {
-                    while (paused) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            return;
-                        }
+    @Override
+    public void run() {
+        for (int i = a; i < b; i++) {
+            // Verificación de pausa
+            synchronized (lock) {
+                while (paused) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
                     }
                 }
-
-                if (isPrime(i)) {
-                    primes.add(i);
-                    System.out.println(i);
-                }
             }
-        }
 
-    public void resumeThread() {
-        synchronized (lock){
-            paused = true;
+            if (isPrime(i)) {
+                primes.add(i);
+                System.out.println(i);
+            }
         }
     }
 
     public void pauseThread() {
-        synchronized (lock){
+        synchronized (lock) {
             paused = true;
+        }
+    }
+
+    public void resumeThread() {
+        synchronized (lock) {
+            paused = false;
+            lock.notify();
         }
     }
 
