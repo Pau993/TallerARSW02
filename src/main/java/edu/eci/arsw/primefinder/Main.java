b0262279aafb76be9d38deb55c931e0eb968d95a
nataliapaez993@gmail.com <nataliapaez993@gmail.com>
2025-02-05 15:19:42 -0500

    Numeros Primos

---
+++ src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw.primefinder;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Control control = Control.newControl(); // Se crea una nueva instancia de la clase Control
+        control.start();
+    }
+	
+}
